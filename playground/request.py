2333529fe200b06f092dc2f54e06f9f25dc1cbad
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-03-25 14:10:17 +0700

    request.py using concurrent.futures.Thread

--- playground/request.py
+++ playground/request.py
@@ -1,12 +1,29 @@
+import concurrent.futures
+import time
+
 from requests import Response, get as requests_get
+
+
+class StatusError(Exception):
+    def __init__(self, response: Response):
+        super().__init__(f'Status code {response.status_code}: {response.text}')
+
 
 class Pokemon:
     def __init__(self, _dict: dict):
         # @formatter:off
         self._id: int       = _dict['id']
-        self._name: int     = _dict['name']
+        self._name: str     = _dict['name']
         self._height: int   = _dict['height']
         self._weight: int   = _dict['weight']
+
+        type_list: list = _dict['types']
+        self._type_1: str   = type_list[0]['type']['name']
+        if len(type_list) == 2:
+            self._type_2: str = type_list[1]['type']['name']
+        else:
+            self._type_2: str = 'null'
+
         self._hp: int       = _dict['stats'][0]['base_stat']
         self._atk: int      = _dict['stats'][1]['base_stat']
         self._def: int      = _dict['stats'][2]['base_stat']
@@ -14,18 +31,15 @@ class Pokemon:
         self._sp_def: int   = _dict['stats'][4]['base_stat']
         self._speed: int    = _dict['stats'][5]['base_stat']
         # @formatter:on
+        self._image: str = (f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official'
+                            f'-artwork/{self._id}.png')
 
-    def __repr__(self):
-        return f'Pokemon({self._id}, {self._name}, {self._height}, {self._weight}, {self._hp}, {self._atk}, {self._def}, {self._sp_atk}, {self._sp_def}, {self._speed})'
+    def csv(self) -> str:
+        return (f'{self._id},{self._name},{self._height},{self._weight},{self._type_1},{self._type_2},{self._hp},'
+                f'{self._atk},{self._def},{self._sp_atk},{self._sp_def},{self._speed},{self._image}')
 
-    def id(self):
-        return self._id
 
-    def csv(self):
-        return f'{self._id},{self._name},{self._height},{self._weight},{self._hp},{self._atk},{self._def},{self._sp_atk},{self._sp_def},{self._speed}'
-
-def _getOne(pokemon_id: int) -> str:
-
+def get_pokemon_data(pokemon_id: int) -> str:
     response: Response = requests_get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
 
     if response.status_code == 200:
@@ -33,10 +47,26 @@ def _getOne(pokemon_id: int) -> str:
         pokemon: Pokemon = Pokemon(response_dict)
         return pokemon.csv()
     else:
-        print(f'\033[91;1mFailed to retrieve data for Pokemon with ID \033[93;1m{pokemon_id}\033[0m')
+        raise StatusError(response)
 
-with open('dataset.csv', 'a') as f:
-    f.write('id,name,height,weight,hp,atk,def,sp_atk,sp_def,speed\n')
-    for i in range(1, 50):
-        f.write(f'{_getOne(i)}\n')
 
+def main():
+    with open('pokemon_threading.csv', 'a') as csv:
+        csv.write(f'id,name,height,weight,type1,type2,hp,atk,def,sp_atk,sp_def,speed,image\n')
+
+        def process_pokemon(i):
+            try:
+                csv.write(f'{get_pokemon_data(i)}\n')
+                print(f'\033[92;1m{i} pokemons have been processed\033[0m')
+            except StatusError as e:
+                print(f'\033[91;1mError processing Pokemon {i}: {e}\033[0m')
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(process_pokemon, range(1, 1027))
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    end_time = time.time()
+    print(f'\033[0mIt take {end_time - start_time}s to complete!\033[94;1m')
