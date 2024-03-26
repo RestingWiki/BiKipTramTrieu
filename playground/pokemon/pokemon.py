c76a87fba77d87e18aee27844b099c28f018a98b
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-03-26 10:16:32 +0700

    Add type hints for property and Optimize csv() method

--- playground/pokemon/pokemon.py
+++ playground/pokemon/pokemon.py
@@ -1,28 +1,34 @@
+from typing import Any, Dict, List
+
+
 class Pokemon:
-    def __init__(self, _dict: dict):
+    def __init__(self, _dict: Dict[str, Any]) -> None:
         # @formatter:off
         self._id: int       = _dict['id']
         self._name: str     = _dict['name']
         self._height: int   = _dict['height']
         self._weight: int   = _dict['weight']
 
-        type_list: list = _dict['types']
-        self._type_1: str   = type_list[0]['type']['name']
-        if len(type_list) == 2:
-            self._type_2: str = type_list[1]['type']['name']
-        else:
-            self._type_2: str = 'null'
+        types: List[Dict[str, Any]] = _dict['types']
+        self._type_1: str = types[0]['type']['name']
+        self._type_2: str = types[1]['type']['name'] if len(types) > 1 else 'null'
 
-        self._hp: int       = _dict['stats'][0]['base_stat']
-        self._atk: int      = _dict['stats'][1]['base_stat']
-        self._def: int      = _dict['stats'][2]['base_stat']
-        self._sp_atk: int   = _dict['stats'][3]['base_stat']
-        self._sp_def: int   = _dict['stats'][4]['base_stat']
-        self._speed: int    = _dict['stats'][5]['base_stat']
+        stats: List[Dict[str, Any]] = _dict['stats']
+        self._hp: int       = stats[0]['base_stat']
+        self._atk: int      = stats[1]['base_stat']
+        self._def: int      = stats[2]['base_stat']
+        self._sp_atk: int   = stats[3]['base_stat']
+        self._sp_def: int   = stats[4]['base_stat']
+        self._speed: int    = stats[5]['base_stat']
+
+        self._image: str = (f'https://raw.githubusercontent.com/PokeAPI/sprites/'
+                            f'master/sprites/pokemon/other/official-artwork/{self._id}.png')
         # @formatter:on
-        self._image: str = (f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official'
-                            f'-artwork/{self._id}.png')
 
     def csv(self) -> str:
-        return (f'{self._id},{self._name},{self._height},{self._weight},{self._type_1},{self._type_2},{self._hp},'
-                f'{self._atk},{self._def},{self._sp_atk},{self._sp_def},{self._speed},{self._image}')
+        return ','.join(str(value) for value in self.__dict__.values())
+
+    # def label(self) -> str:
+    #     return ','.join(
+    #         attr[1:] for attr in dir(self) if not attr.startswith('_') and not callable(getattr(self, attr))
+    #     )
