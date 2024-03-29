f45480b8d991d9b555e70c395e60161a7e506233
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-03-29 13:10:42 +0700

    Remove redundant comment

--- playground/PokemonType/request.py
+++ playground/PokemonType/request.py
@@ -53,15 +53,3 @@ if __name__ == "__main__":
     main()
     print(f'\033[0mIt take {time.perf_counter() - start_time}s to complete!\033[94;1m')
 
-# with open('pokemon_threading.csv', 'a') as csv:
-#     csv.write(f'id,name,base_exp,height,weight,type1,type2,hp,atk,def,sp_atk,sp_def,speed,image\n')
-#
-#     def process_pokemon(i):
-#         try:
-#             csv.write(f'{get_Pokemon(i)}\n')
-#             print(f'\033[92;1m{i} pokemons have been processed\033[0m')
-#         except StatusError as e:
-#             print(f'\033[91;1mError processing Pokemon {i}: {e}\033[0m')
-#
-#     with concurrent.futures.ThreadPoolExecutor() as executor:
-#         executor.map(process_pokemon, range(1, 1027))
