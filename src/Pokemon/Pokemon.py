8c97e3d04fa5fe5b001b7a2a2c6a430f5c3fdfe2
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-04-11 22:09:31 +0700

    Module Pokemon

---
+++ src/Pokemon/Pokemon.py
@@ -0,0 +1,48 @@
+class Pokemon:
+
+    def __init__(self, _id):
+        # @formatter:off
+        self._id        = _id
+        self._name      =
+        self._name      =
+        self._type1     =
+        self._type2     =
+        self._hp        =
+        self._atk       =
+        self._def       =
+        self._speed     =
+        self._sp_atk    =
+        self._sp_def    =
+        self._image     =
+        self._status    =
+        # @formatter:on
+
+    def show_image(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def taken_damage(self, damage_taken):
+        pass
+
+    def defense(self, damage_taken):
+        pass
+
+    def use_skill_1(self, num):
+        pass
+
+    def use_skill_2(self, num):
+        pass
+
+    def use_item(self, item: Item):
+        pass
+
+    def run(self):
+        pass
+
+    def interact(self):
+        pass
+
+    def 
+
